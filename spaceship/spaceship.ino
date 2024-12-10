c58675275ac5fbaef11d3f1d8455e4faf54b4b51
Yashar Honarmandi <yashar@honarmandi.se>
2024-12-10 22:05:52 +0100

    Add setup and global switchstate variable

--- spaceship/spaceship.ino
+++ spaceship/spaceship.ino
@@ -1,6 +1,11 @@
+int switchState = 0;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 void loop() {
