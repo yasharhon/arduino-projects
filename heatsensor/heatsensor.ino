f8cea5a38840ef5db91d7865b4010fad38c62831
Yashar Honarmandi <yashar@honarmandi.se>
2024-12-14 01:48:45 +0100

    Print sensor voltage

--- heatsensor/heatsensor.ino
+++ heatsensor/heatsensor.ino
@@ -1,5 +1,7 @@
 const int sensorPin = A0;
 const float ambientTemp = 20.0;
+const float maxAnalogReadValue = 1024.0;
+const float maxVoltage = 5.0;
 
 void setup ()
 {
@@ -12,10 +14,18 @@ void setup ()
   }
 }
 
+float analogToVoltage(float analogVal)
+{
+  return (analogVal / maxAnalogReadValue) * maxVoltage;
+}
+
 void loop()
 {
   int sensorVal = analogRead(sensorPin);
 
   Serial.print("Sensor value: ");
   Serial.print(sensorVal);
+
+  Serial.print(". Voltage: ");
+  Serial.print(analogToVoltage(sensorVal));
 }
