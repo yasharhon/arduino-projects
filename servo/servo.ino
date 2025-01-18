aaa2c1d6ca37f5685d459eeb3c583a7429f1aed5
Yashar Honarmandi <yashar@honarmandi.se>
2025-01-19 00:29:01 +0100

    Compute and print servo angle

--- servo/servo.ino
+++ servo/servo.ino
@@ -4,6 +4,7 @@ int const potPin = A0;
 int const servoPin = 9;
 
 int potVal;
+int angle;
 
 Servo myServo;
 
@@ -17,8 +18,11 @@ Servo myServo;
 void loop() {
   // put your main code here, to run repeatedly:
   potVal = analogRead(potPin);
+  angle = map(potVal, 0, 1023, 0, 179);
 
   Serial.print("Potentiometer: ");
   Serial.print(potVal);
+  Serial.print(". Angle: ");
+  Serial.print(angle);
   Serial.println();
 }
