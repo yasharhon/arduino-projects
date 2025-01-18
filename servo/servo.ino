0151cf0667bd95b93924f881237859e47167b424
Yashar Honarmandi <yashar@honarmandi.se>
2025-01-19 00:08:47 +0100

    Add servo setup and constants

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,6 +1,15 @@
+#include <Servo.h>
+
+int const potPin = A0;
+int const servoPin = 9;
+
+Servo myServo;
+
  void setup() {
   // put your setup code here, to run once:
+  myServo.attach(servoPin);
 
+  Serial.begin(9600);
 }
 
 void loop() {
